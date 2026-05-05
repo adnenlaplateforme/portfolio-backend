64a2fc0ecc0fd388030e1febdd92f4ebca888b2f
adnensaid83 <adnensaid83@gmail.com>
2026-05-05 15:07:16 +0200

    feat: add authorize middleware

---
+++ src/middlewares/authorize.middleware.ts
@@ -0,0 +1,13 @@
+import type { Request, Response, NextFunction } from 'express';
+import AppError from '../errors/AppError.ts';
+
+const authorize = (...roles: string[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    if (!req.user || !roles.includes(req.user.role)) {
+      return next(new AppError('Forbidden', 403));
+    }
+    next();
+  };
+};
+
+export default authorize;
